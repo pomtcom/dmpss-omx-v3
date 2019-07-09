553a3ef0bee51bf905e9bc06652c59efe24e9e26
Tawatchai Phetdumrongsakul <tawatchaiphetdumrongsakul@tdg-m0031.local>
2019-07-09 13:47:52 +0700

    testcheckoutscm

--- deployment/jenkins.groovy
+++ deployment/jenkins.groovy
@@ -19,29 +19,33 @@ node {
         vault.init();
         def mysecret = vault.getSecret('mysql', 'mysql-omx-instance1');
         print('mysecret is ' + mysecret) ;
-    }
-    stage('Checkout test'){
-        print('test checkout');
-        checkout scm ;
-        print('checkout is completed');
-    }
-    stage('Read .yaml test'){
-        mydata = readYaml file: "secrettest.yml"
 
-        print('test read value is ' + mydata.type);
+        vault.checkOutSecretTemplate();
+
+
     }
-    stage('test write ymal'){
-        def amap = ['something': 'my datas','size': 3, 'isEmpty': false]
-        writeYaml file: 'datas.yaml', data: amap
-    }
-    stage('test yaml2'){
-        mydata = readYaml file: "secrettest.yml"
-        //modify
-        mydata.info = "b" ;
-        mydata.data.TESTKEY2 = "key2" ;
-        mydata.data.TESTKEY3 = "key3" ;
-        writeYaml file: 'newtest.yaml', data: mydata ;
-    }
+    // stage('Checkout test'){
+    //     print('test checkout');
+    //     checkout scm ;
+    //     print('checkout is completed');
+    // }
+    // stage('Read .yaml test'){
+    //     mydata = readYaml file: "secrettest.yml"
+
+    //     print('test read value is ' + mydata.type);
+    // }
+    // stage('test write ymal'){
+    //     def amap = ['something': 'my datas','size': 3, 'isEmpty': false]
+    //     writeYaml file: 'datas.yaml', data: amap
+    // }
+    // stage('test yaml2'){
+    //     mydata = readYaml file: "secrettest.yml"
+    //     //modify
+    //     mydata.info = "b" ;
+    //     mydata.data.TESTKEY2 = "key2" ;
+    //     mydata.data.TESTKEY3 = "key3" ;
+    //     writeYaml file: 'newtest.yaml', data: mydata ;
+    // }
     
 
 }
